bb3c31b2bb66f0f5126df38605345de19afbf7c3
Zack Bostock <zbostock56@gmail.com>
2022-11-18 13:15:38 -0500

    More documentation, fixed counting bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Format:
+# Piece Format:
 # Zeros for no pieces
 # Letters for other pieces
 # Legend:
@@ -11,19 +11,45 @@
 # 0 -> No piece
 # CAPITALS: white
 # lowercase: black
+# Standard Layout:
+# rnbqkbnr
+# pppppppp
+# 00000000
+# 00000000
+# 00000000
+# 00000000
+# PPPPPPPP
+# RNBQKBNR
 
-def function():
+# Next Move Format:
+# After pieces: "Black" or "White" indicates next move
+
+# Castling Rights Format:
+# After next move descriptor, 'K' 'Q' 'k' 'q' indicates which side has not castled
+# e.g. ~ Kkq : King side of white, King and Queen side of black all have castling rights
+
+# Halfmove Clock
+# After Castling Rights descriptor, some number up to 50 moves, referring to the
+# 50 move draw rule describes the number of moves that have been taken without a capture or after pawn move
+# Resets after a capture or a pawn has been moved
+
+# Fullmove Counter
+# After Halfmove descriptor, some number incremeneted by 1 referring to the number
+# of moves by each player, incremented up after each of Black's move
+
+linearray = []
+fenString = ""
+def pieceFen():
     # Ensure the file exists
     try:
         #filename = input("Insert file name:\n")
-        fp = open("test.txt", "r+")
+        fp = open("test2.txt", "r+")
     except:
         print("No such file exists")
     # read all lines from file
     linearray = fp.readlines()
     counter = 0
-    fenString = ""
-    while counter < len(linearray):
+    while counter < 8:
         line = linearray[counter]
         zeroCounter = 0
         i = 0
@@ -31,6 +57,7 @@ def function():
             # If the part of the string is not a zero, add it to the return string
             if (line[i] != "0" and line[i] != "\n"):
                 fenString += line[i]
+                i += 1
             # If the part of the string is a zero, count all of them until there are not any more
             # BUG: Misses adding the piece right after the end of a run of zeros
             elif (line[i] == "0"):
@@ -38,15 +65,15 @@ def function():
                     zeroCounter += 1
                 fenString += str(zeroCounter)
                 i += zeroCounter
-                if (i >= len(line) - 1):
-                    fenString += "/"
                 zeroCounter = 0
             else:
                 if (counter != 7):
                     fenString += "/"
-            i += 1
+                i += 1    
         counter += 1
     print(fenString)
+def castlingFen():
+    if (len(linearray) != 0):
+        print()    
 
-
-function()
+pieceFen()
